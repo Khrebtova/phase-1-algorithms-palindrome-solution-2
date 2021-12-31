25ae6361168e0d8c74127f5a229129440ae2653d
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2021-12-31 15:36:26 -0800

    commit tree b50a568477f147b94b65f9ee265a45e458aab870

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+
+//function will compare first part of the input word wth second part woth the input and if theu same return TRUE, if not terurn FALSE
 function isPalindrome(word) {
-  // Write your algorithm here
+  // iterate from the beginning of the string to the middle of the string
+   //i and j are coresponding leter indexesfrom begining and end of our word.  
+  for (let i= 0; i < word.length/2; i++){
+    let j = word.length-1-i
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    if (word[i] !== word[j])
+    {
+      //if leters don't mutch return 'false'
+      return false
+    }
+  }
+  // if we reach the middle, and all the letters match, return true
+  return true
 }
+
+
 
 /* 
   Add your pseudocode here
